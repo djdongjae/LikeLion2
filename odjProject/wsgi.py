c9b7be8dcaa44846129b9ecc860502b8459b0295
djdongjae <isf1999@naver.com>
2023-07-09 19:24:36 +0900

    push my project

---
+++ odjProject/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for odjProject project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'odjProject.settings')
+
+application = get_wsgi_application()
